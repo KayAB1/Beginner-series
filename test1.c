6c4bc0ebbffc845655969aa525a44a8c41099090
KayAB1 <100501105+KayAB1@users.noreply.github.com>
2022-03-22 18:47:52 +0800

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int main()
+//{
+//	//通过指针来打印/改变数组（通过指针加整数的方式来访问数组）
+//	int arr[10]={0};
+//	int*p=arr;
+//	int i=0;
+//	for(i=0;i<10;i++)
+//	{
+//		printf("%d",*(p+i)=1);
+//
+//	}
+//
+//	return 0;
+//}
