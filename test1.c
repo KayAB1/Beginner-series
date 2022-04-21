8a83e99779865ee1430d8f010f5bdb5b7fd921f2
KayAB1 <100501105+KayAB1@users.noreply.github.com>
2022-04-21 19:25:25 +0800

    Add files via upload

--- test1.c
+++ test1.c
@@ -1,4 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+
 
 ////匿名结构体类型（无结构体标签）
 //
@@ -33,11 +37,62 @@
 //};
 
 
-struct Node//结构体内包含一个结构体指针是可以的（似链表，一个是数值域，一个是指针域(存下一个数值的地址)）----正确的自引用方式
+//struct Node//结构体内包含一个结构体指针是可以的（似链表，一个是数值域，一个是指针域(存下一个数值的地址)）----正确的自引用方式
+//{
+//	int data;
+//	struct Node*next;
+//
+//
+//};
+
+
+//*****************************************************************************************************************************
+
+//结构体的内存对齐
+
+//结构体未嵌套结构体的结构体内存对齐
+//struct S1
+//{
+//	char c1;
+//	int a;
+//	char c2;
+//
+//};
+//
+//
+//int main()
+//{
+//	struct S1 s1={0};
+//	printf("%d\n",sizeof(s1));
+//
+//
+//}
+
+
+
+
+//结构体嵌套结构体的结构体内存对齐
+
+struct S3
 {
-	int data;
-	struct Node*next;
+	double d;
+	char c;
+	int i;
+};
+
+
+struct S4
+{
+	char c1;
+	struct S3 s3;
+	double d;
+};
+
+
+int main ()
+{
+	printf("%d\n",sizeof(struct S3));//16
+	printf("%d\n",sizeof(struct S4));
 
 
 }
-
